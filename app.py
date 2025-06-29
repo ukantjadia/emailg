f2626e9539b90d281ae3ac91489ee8d79f1623a5
Ridhun ks <140372564+ridhzeus@users.noreply.github.com>
2025-06-29 15:08:49 +0530

    Minor errors"routes"

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes.input_form_routes import input_form
 from routes.main_routes import main_bp
 from logging_setup import setup_logging     # Enable logging
 from config.config import config
-from Routes.feedback_routes import feedback_bp
+from routes.feedback_routes import feedback_bp
 def create_app(config_class=config):
     """Create and configure the Flask application"""
     setup_logging()
