0d539430de2bc8fda90f4929745abf96d40030cf
Abhivanth Sivaprakash <abhivanth23110558@snuchennai.edu.in>
2026-04-29 15:45:20 +0530

    Update feedback_routes.py

--- Routes/feedback_routes.py
+++ Routes/feedback_routes.py
@@ -8,10 +8,6 @@ feedback_controller = FeedbackController()
 def capture_feedback():
     return jsonify(feedback_controller.capture_feedback(request.json))
 
-@feedback_bp.route('/regenerate', methods=['POST'])
-def regenerate_content():
-    return jsonify(feedback_controller.handle_regeneration(request.json))
-
 @feedback_bp.route('/analytics', methods=['GET'])
 def get_analytics():
     filters = request.args.to_dict()
